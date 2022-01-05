8862011273cc66fd6e31b02e2134dd45503c82d3
hculek <hrvoje.culek@gmail.com>
2022-01-05 12:25:23 +0100

    Initial commit.

---
+++ Library Persistence/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Persistence
+{
+    //https://stackoverflow.com/questions/1216837/c-sharp-generic-crud-operation
+
+
+    public class GenericRepository { //obrisati taj redak
+    //public class GenericRepository<T> : IGenericRepository<T> where T : class
+    //{
+        //private EmployeeDBContext _context = null;
+        //private DbSet<T> table = null;
+        //public GenericRepository()
+        //{
+        //    this._context = new EmployeeDBContext();
+        //    table = _context.Set<T>();
+        //}
+        //public GenericRepository(EmployeeDBContext _context)
+        //{
+        //    this._context = _context;
+        //    table = _context.Set<T>();
+        //}
+        //public IEnumerable<T> GetAll()
+        //{
+        //    return table.ToList();
+        //}
+        //public T GetById(object id)
+        //{
+        //    return table.Find(id);
+        //}
+        //public void Insert(T obj)
+        //{
+        //    table.Add(obj);
+        //}
+        //public void Update(T obj)
+        //{
+        //    table.Attach(obj);
+        //    _context.Entry(obj).State = EntityState.Modified;
+        //}
+        //public void Delete(object id)
+        //{
+        //    T existing = table.Find(id);
+        //    table.Remove(existing);
+        //}
+        //public void Save()
+        //{
+        //    _context.SaveChanges();
+        //}
+    }
+}
