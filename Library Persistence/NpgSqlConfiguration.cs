52426d4c4a633791988fb4f13623f67da63f4793
hculek <hrvoje.culek@gmail.com>
2022-01-17 12:10:24 +0100

    Updatet Persistence layer.

---
+++ Library Persistence/NpgSqlConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+using System.Data.Entity;
+
+namespace Library_Persistence
+{
+    class NpgSqlConfiguration : DbConfiguration
+    {
+        //https://www.npgsql.org/ef6/index.html#basic-configuration
+        public NpgSqlConfiguration()
+        {
+            var name = "Npgsql";
+
+            SetProviderFactory(providerInvariantName: name,
+                               providerFactory: NpgsqlFactory.Instance);
+
+            SetProviderServices(providerInvariantName: name,
+                                provider: NpgsqlServices.Instance);
+
+            SetDefaultConnectionFactory(connectionFactory: new NpgsqlConnectionFactory());
+        }
+    }
+}
