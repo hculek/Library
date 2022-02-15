96f13537481d3740f7ff080384bd0d098a970ee1
hculek <hrvoje.culek@gmail.com>
2022-02-15 12:09:08 +0100

    Property update.

--- Library Domain/Objects/Genre/Genre.cs
+++ Library Domain/Objects/Genre/Genre.cs
@@ -23,7 +23,7 @@ namespace Library_Domain.Objects
 
 
         //https://docs.microsoft.com/en-gb/ef/ef6/querying/related-data?redirectedfrom=MSDN
-        //public virtual ICollection<Book.Book> Books { get; set; } baca error
+        //public virtual ICollection<Book> Books { get; set; } baca error
 
         public virtual ICollection<Book> Books { get; set; }
     }
