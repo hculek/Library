f7cd35db8e82698c4e7e402f9d2895f80c38580d
hculek <hrvoje.culek@gmail.com>
2022-02-17 11:49:00 +0100

    POCO data annotation update.

--- Library Domain/Objects/Book/Book.cs
+++ Library Domain/Objects/Book/Book.cs
@@ -24,10 +24,8 @@ namespace Library_Domain.Objects
         [Column("book_total_pages"), Display(Name = "Total Pages")]
         public int BookTotalPages { get; set; }
 
-        [ForeignKey("author_id")]
-        public ICollection<Author> Authors { get; set; }
+        public virtual ICollection<Author> Authors { get; set; }
 
-        [ForeignKey("genre_id")]
-        public ICollection<Genre> Genres { get; set; }
+        public virtual ICollection<Genre> Genres { get; set; }
     }
 }
