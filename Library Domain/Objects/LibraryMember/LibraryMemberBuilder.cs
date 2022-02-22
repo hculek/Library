7e9eeceb607a5cf9948c06b77b345a6e939081af
hculek <hrvoje.culek@gmail.com>
2022-02-22 14:42:20 +0100

    Temporary commented out, to be resolved later.

--- Library Domain/Objects/LibraryMember/LibraryMemberBuilder.cs
+++ Library Domain/Objects/LibraryMember/LibraryMemberBuilder.cs
@@ -1,52 +1,52 @@
-﻿namespace Library_Domain.Objects.LibraryMember
-{
-    class LibraryMemberBuilder : ILibraryMember
-    {
-        LibraryMember member = new LibraryMember();
+﻿//namespace Library_Domain.Objects.LibraryMember
+//{
+//    class LibraryMemberBuilder : ILibraryMember
+//    {
+//        LibraryMember member = new LibraryMember();
 
-        public LibraryMemberBuilder()
-        {
-            this.Reset();
-        }
+//        public LibraryMemberBuilder()
+//        {
+//            this.Reset();
+//        }
 
-        public void Adress(string Adress)
-        {
-            this.member.adress = Adress;
-        }
+//        public void Adress(string Adress)
+//        {
+//            this.member.adress = Adress;
+//        }
 
-        public void Email(string Email)
-        {
-            this.member.email = Email;
-        }
+//        public void Email(string Email)
+//        {
+//            this.member.email = Email;
+//        }
 
-        public void FirstName(string FirstName)
-        {
-            this.member.firstname = FirstName;
-        }
+//        public void FirstName(string FirstName)
+//        {
+//            this.member.firstname = FirstName;
+//        }
 
-        public void LastName(string LastName)
-        {
-            this.member.lastname = LastName;
-        }
+//        public void LastName(string LastName)
+//        {
+//            this.member.lastname = LastName;
+//        }
 
-        public void LibraryMemberID(long LibraryMemberID)
-        {
-            this.member.librarymemberid = LibraryMemberID;
-        }
+//        public void LibraryMemberID(long LibraryMemberID)
+//        {
+//            this.member.librarymemberid = LibraryMemberID;
+//        }
 
-        public void MiddleName(string MiddleName)
-        {
-            this.member.middlename = MiddleName;
-        }
+//        public void MiddleName(string MiddleName)
+//        {
+//            this.member.middlename = MiddleName;
+//        }
 
-        public void Reset()
-        {
-            this.member = new LibraryMember();
-        }
+//        public void Reset()
+//        {
+//            this.member = new LibraryMember();
+//        }
 
-        public void Telephone(string Telephone)
-        {
-            this.member.telephone = Telephone;
-        }
-    }
-}
+//        public void Telephone(string Telephone)
+//        {
+//            this.member.telephone = Telephone;
+//        }
+//    }
+//}
