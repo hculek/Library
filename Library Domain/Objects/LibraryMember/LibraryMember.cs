ba68f8af84ac839f1ba08e550adffeaab9f5e819
hculek <hrvoje.culek@gmail.com>
2022-02-22 11:41:07 +0100

    Added date properties. Updated data annotations.

--- Library Domain/Objects/LibraryMember/LibraryMember.cs
+++ Library Domain/Objects/LibraryMember/LibraryMember.cs
@@ -9,15 +9,40 @@ namespace Library_Domain.Objects.LibraryMember
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long librarymemberid { get; set; }
-        public string firstname {get; set;}
-        public string middlename { get; set; }
-        public string lastname { get; set; }
-        public string adress { get; set; }
-        public string email { get; set; }
-        public string telephone { get; set; }
+        [Column("member_id"), Display(Name = "Member ID")]
+        public long LibraryMemberID { get; set; }
 
-        public DateTime membershipstartdate { get; set; }
+        [Column("first_name"), Display(Name = "First Name")]
+        [Required(ErrorMessage = "First name is required.")]
+        public string FirstName {get; set;}
+
+        [Column("middle_name"), Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Column("last_name"), Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last name is required.")]
+        public string Lastname { get; set; }
+
+        [Column("adress"), Display(Name = "Adress")]
+        public string Adress { get; set; }
+
+        [Column("email"), Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Column("phone_number"), Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Column("membership_start_date"), Display(Name = "Membership start date")]
+        public DateTime MembershipStartDate { get; set; }
+
+        [Column("membership_renewal_date"), Display(Name = "Membership renewal date")]
+        public DateTime MembershipRenewalDate { get; set; }
+
+        [Column("membership_expiry_date"), Display(Name = "Membership expiry date")]
+        public DateTime MembershipExpiryDate { get; set; }
+
+        [Column("membership_active_status"), Display(Name = "Membership active status")]
+        bool MembershipStatus { get; set; }
 
     }
 }
