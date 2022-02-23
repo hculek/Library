0cda3d9b75054f8945a051966228ce96d4e997d6
hculek <hrvoje.culek@gmail.com>
2022-02-23 08:33:46 +0100

    Added methods for properties.

--- Library Domain/IRepositories/ILibraryMemberRepository.cs
+++ Library Domain/IRepositories/ILibraryMemberRepository.cs
@@ -1,4 +1,4 @@
-﻿using Library_Domain.Objects.LibraryMember;
+﻿using Library_Domain.Objects;
 namespace Library_Domain.dbInterfaces
 {
     public interface ILibraryMemberRepository : IGenericRepository<LibraryMember>
