23977d0a2f7ece996aad16069e932393a8280408
hculek <hrvoje.culek@gmail.com>
2022-02-23 08:35:26 +0100

    Initial comit.

---
+++ Library Service/Builders/LibraryMemberBuilder.cs
@@ -0,0 +1,79 @@
+﻿using Library_Domain.Objects;
+using System;
+
+namespace Library_Service.Builders
+{
+    class LibraryMemberBuilder : ILibraryMember
+    {
+        private LibraryMember _libraryMember = new LibraryMember();
+
+
+        public LibraryMemberBuilder() 
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this._libraryMember = new LibraryMember();
+        }
+
+        public LibraryMember Build()
+        {
+            LibraryMember result = this._libraryMember;
+            this.Reset();
+            return result;
+        }
+
+        public void Adress(string Adress)
+        {
+            this._libraryMember.Adress = Adress;
+        }
+
+        public void Email(string Email)
+        {
+            this._libraryMember.Email = Email;
+        }
+
+        public void FirstName(string FirstName)
+        {
+            this._libraryMember.FirstName = FirstName;
+        }
+
+        public void LastName(string LastName)
+        {
+            this._libraryMember.Lastname = LastName;
+        }
+
+        public void LibraryMemberID(long LibraryMemberID)
+        {
+            this._libraryMember.LibraryMemberID = LibraryMemberID;
+        }
+
+        public void MembershipExpiryDate(DateTime MembershipExpiryDate)
+        {
+            this._libraryMember.MembershipExpiryDate = MembershipExpiryDate;
+        }
+
+        public void MembershipRenewalDate(DateTime MembershipRenewalDate)
+        {
+            this._libraryMember.MembershipRenewalDate = MembershipRenewalDate;
+        }
+
+        public void MemberShipStartDate(DateTime MemberShipStartDate)
+        {
+            this._libraryMember.MembershipStartDate = MemberShipStartDate;
+        }
+
+        public void MiddleName(string MiddleName)
+        {
+            this._libraryMember.MiddleName = MiddleName;
+        }
+
+        public void PhoneNumber(string PhoneNumber)
+        {
+            this._libraryMember.PhoneNumber = PhoneNumber;
+        }
+    }
+
+}
