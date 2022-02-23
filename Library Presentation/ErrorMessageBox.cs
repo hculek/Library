9cb5250c31a36f3e3f15fe08a3ac5b756bd21738
hculek <hrvoje.culek@gmail.com>
2022-02-23 15:02:20 +0100

    Initial comit.

---
+++ Library Presentation/ErrorMessageBox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Library_Presentation
+{
+    public static class ErrorMessageBox
+    {
+        public static void Show(Exception ex) 
+        {
+            MessageBox.Show(ex.Message.ToString());
+        }
+    }
+}
