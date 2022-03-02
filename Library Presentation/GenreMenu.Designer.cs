849b63662b074073f380272807faa4539b6a426e
Hrvoje Culek <hhcc>
2022-03-02 20:11:56 +0100

    Cleanup.

--- Library Presentation/GenreMenu.Designer.cs
+++ Library Presentation/GenreMenu.Designer.cs
@@ -132,6 +132,7 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(22, 163);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
